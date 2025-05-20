e487d5f30d2fd0e7101f9eac9200532bf5dc3227
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-05-20 03:15:35 -0400

    Add HTML elements and e-mail validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 import "./styles.css";
+
+const form = document.querySelector("form");
+const emailInput = document.querySelector("#email");
+const emailError = document.querySelector("#email-error");
+
+const emailRegeX =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+const isValidEmail = () => {
+  const validity =
+    emailInput.value.length !== 0 && emailRegeX.test(emailInput.value);
+
+  return validity;
+};
+
+const setEmailClass = (isValid) => {
+  emailInput.className = isValid ? "valid" : "invalid";
+};
+
+const setEmailError = (isValid) => {
+  if (isValid) {
+    emailError.textContent = "";
+    emailError.removeAttribute("class");
+  } else {
+    emailError.textContent = "E-Mail is invalid.";
+    emailError.setAttribute("class", "active");
+  }
+};
+
+const initializeEmail = () => {
+  const emailInput = isValidEmail();
+  setEmailClass(emailInput);
+};
+
+const handleEmailInput = () => {
+  const emailInput = isValidEmail();
+  setEmailClass(emailInput);
+  setEmailError(emailInput);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const emailInput = isValidEmail();
+  setEmailClass(emailInput);
+  setEmailError(emailInput);
+};
+
+window.addEventListener("load", initializeEmail);
+emailInput.addEventListener("blur", handleEmailInput);
+form.addEventListener("submit", handleSubmit);
