7114b62f4392394a7291aa14e86a553e5d5e038e
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-05-21 03:38:30 -0400

    Add initial password/confirmation validation

--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ const emailInput = document.querySelector("#email");
 const emailError = document.querySelector("#email-error");
 const postalCodeInput = document.querySelector("#postal-code");
 const postalCodeError = document.querySelector("#postal-code-error");
+const passwordInput = document.querySelector("#password");
+const passwordError = document.querySelector("#password-error");
+const passwordConfirmationInput = document.querySelector(
+  "#password-confirmation",
+);
+const passwordConfirmationError = document.querySelector(
+  "#password-confirmation-error",
+);
 
-//E-Mail Checking
+// E-Mail Checking
 
 const isValidEmailLength = () => {
   const validity = emailInput.value.length !== 0;
@@ -49,6 +57,8 @@ const setEmailError = (isValid) => {
   }
 };
 
+// Postal Code Checking
+
 const isValidPostalCodeLength = () => {
   const validity = postalCodeInput.value.length !== 0;
   return validity;
@@ -71,9 +81,6 @@ const isValidPostalCodePattern = () => {
   const countryInput = document.querySelector("#country").value;
   const postalCodeInput = document.querySelector("#postal-code").value;
 
-  console.log(postalCodeConstraints[countryInput][0]);
-  console.log(postalCodeInput);
-
   const validity = postalCodeConstraints[countryInput][0].test(
     postalCodeInput.toString(),
   );
@@ -85,8 +92,7 @@ const isValidPostalCode = () => {
   const postalCodeLength = isValidPostalCodeLength();
   const postalCodePattern = isValidPostalCodePattern();
   const validity = postalCodeLength && postalCodePattern;
-  console.log(postalCodeLength);
-  console.log(postalCodePattern);
+
   return validity;
 };
 
@@ -111,6 +117,98 @@ const setPostalCodeError = (isValid) => {
   }
 };
 
+// Password Checking
+
+const isValidPasswordLength = () => {
+  const validity = passwordInput.value.length !== 0;
+  return validity;
+};
+
+const isValidPasswordStrength = () => {
+  const passwordInput = document.querySelector("#password").value;
+  const passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
+
+  const validity = passwordRegEx.test(passwordInput);
+  return validity;
+};
+
+const isValidPassword = () => {
+  const passwordLength = isValidPasswordLength();
+  const passwordStrength = isValidPasswordStrength();
+
+  const validity = passwordLength && passwordStrength;
+  return validity;
+};
+
+const setPasswordClass = (isValid) => {
+  passwordInput.className = isValid ? "valid" : "invalid";
+};
+
+const setPasswordError = (isValid) => {
+  const passwordLength = isValidPasswordLength();
+  const passwordStrength = isValidPasswordStrength();
+
+  if (isValid) {
+    passwordError.textContent = "";
+    passwordError.removeAttribute("class");
+  } else if (!passwordLength) {
+    passwordError.textContent = "Please enter a password.";
+    passwordError.setAttribute("class", "active");
+  } else if (!passwordStrength) {
+    passwordError.textContent =
+      "Please enter a stronger password. (8+ Characters, 1 Uppercase, 1 Lowercase, 1 Digit, 1 Special Character)";
+    passwordError.setAttribute("class", "active");
+  }
+};
+
+// Password Confirmation Checking
+
+const isValidPasswordConfirmationLength = () => {
+  const validity = passwordConfirmationInput.value.length !== 0;
+  return validity;
+};
+
+const isValidPasswordConfirmationMatch = () => {
+  const passwordValue = document.querySelector("#password").value;
+  const passwordConfirmationValue = document.querySelector(
+    "#password-confirmation",
+  ).value;
+
+  const validity = passwordValue === passwordConfirmationValue;
+  return validity;
+};
+
+const isValidPasswordConfirmation = () => {
+  const passwordConfirmationLength = isValidPasswordConfirmationLength();
+  const passwordConfirmationMatch = isValidPasswordConfirmationMatch();
+
+  const validity = passwordConfirmationLength && passwordConfirmationMatch;
+  return validity;
+};
+
+const setPasswordConfirmationClass = (isValid) => {
+  passwordConfirmationInput.className = isValid ? "valid" : "invalid";
+};
+
+const setPasswordConfirmationError = (isValid) => {
+  const passwordConfirmationLength = isValidPasswordConfirmationLength();
+  const passwordConfirmationMatch = isValidPasswordConfirmationMatch();
+
+  if (isValid) {
+    passwordConfirmationError.textContent = "";
+    passwordConfirmationError.removeAttribute("class");
+  } else if (!passwordConfirmationLength) {
+    passwordConfirmationError.textContent =
+      "Please enter password confirmation.";
+    passwordConfirmationError.setAttribute("class", "active");
+  } else if (!passwordConfirmationMatch) {
+    passwordConfirmationError.textContent =
+      "Password confirmation does not match password.";
+    passwordConfirmationError.setAttribute("class", "active");
+  }
+};
+
+// Initialization to set the class of inputs on page load.
 const initializeEmail = () => {
   const emailInput = isValidEmail();
   setEmailClass(emailInput);
@@ -121,6 +219,17 @@ const initializePostalCode = () => {
   setPostalCodeClass(postalCodeInput);
 };
 
+const initializePassword = () => {
+  const passwordInput = isValidPassword();
+  setPasswordClass(passwordInput);
+};
+
+const initializePasswordConfirmation = () => {
+  const passwordConfirmationInput = isValidPasswordConfirmation();
+  setPasswordConfirmationClass(passwordConfirmationInput);
+};
+
+// Handles actions whenever the value of inputs changes.
 const handleEmailInput = () => {
   const emailInput = isValidEmail();
   setEmailClass(emailInput);
@@ -133,22 +242,48 @@ const handlePostalCodeInput = () => {
   setPostalCodeError(postalCodeInput);
 };
 
-// Checks once the user clicks the submit button
+const handlePasswordInput = () => {
+  const passwordInput = isValidPassword();
+  setPasswordClass(passwordInput);
+  setPasswordError(passwordInput);
+};
+
+const handlePasswordConfirmationInput = () => {
+  const passwordConfirmationInput = isValidPasswordConfirmation();
+  setPasswordConfirmationClass(passwordConfirmationInput);
+  setPasswordConfirmationError(passwordConfirmationInput);
+};
+
+// Checks that run once the user clicks the submit button.
 const handleSubmit = (event) => {
   event.preventDefault();
 
   const emailInput = isValidEmail();
   const postalCodeInput = isValidPostalCode();
+  const passwordInput = isValidPassword();
+  const passwordConfirmationInput = isValidPasswordConfirmation();
   setEmailClass(emailInput);
   setEmailError(emailInput);
   setPostalCodeClass(postalCodeInput);
   setPostalCodeError(postalCodeInput);
+  setPasswordClass(passwordInput);
+  setPasswordError(passwordInput);
+  setPasswordConfirmationClass(passwordConfirmationInput);
+  setPasswordConfirmationError(passwordConfirmationInput);
 };
 
+// The code that runs the first time the page loads/refreshes.
 window.addEventListener("load", () => {
   initializeEmail();
   initializePostalCode();
+  initializePassword();
+  initializePasswordConfirmation();
 });
 emailInput.addEventListener("blur", handleEmailInput);
 postalCodeInput.addEventListener("blur", handlePostalCodeInput);
+passwordInput.addEventListener("blur", handlePasswordInput);
+passwordConfirmationInput.addEventListener(
+  "blur",
+  handlePasswordConfirmationInput,
+);
 form.addEventListener("submit", handleSubmit);
